75c6400683d8cf61562ddbf987e58f3b2d8f228e
Ian Greenfield <iangreenfield@MacBookPro.lan>
2025-07-17 15:31:11 -0500

    update font stylings

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import ThreeColumnTable from '@/components/three-column-table';
-
 export default function Home() {
-  return (
-    <div>
-      <ThreeColumnTable />
-    </div>
-  );
+  return <div></div>;
 }
