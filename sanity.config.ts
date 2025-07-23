8448db523cce964a21ec25d9d1f7aaf9913191b5
Ian Greenfield <iangreenfield@MacBookPro.lan>
2025-07-23 14:04:54 -0500

    update sanity/vision plugin import

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
 import { colorInput } from '@sanity/color-input'
 import { schemaTypes } from './sanity/schemaTypes'
+
+const plugins = [structureTool(), colorInput()]
+
+// Only add visionTool in development
+if (process.env.NODE_ENV === 'development') {
+  const { visionTool } = require('@sanity/vision')
+  plugins.push(visionTool())
+}
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +18,7 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
-  plugins: [structureTool(), visionTool(), colorInput()],
+  plugins,
 
   schema: {
     types: schemaTypes,
