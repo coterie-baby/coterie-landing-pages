1e7a4c2a6fe080eb593b13607733be5ff0e2d131
Ian Greenfield <iangreenfield@MacBookPro.lan>
2025-12-12 14:02:23 -0600

    ugc page initial build out

--- components/flex-announcement-bar.tsx
+++ components/flex-announcement-bar.tsx
@@ -1,13 +1,22 @@
+import { StarRating } from './reviews/star-rating';
+
 interface FlexAnnouncementBarProps {
   announcement?: string;
+  showStars?: boolean;
+  rating?: number;
 }
 
 export default function FlexAnnouncementBar({
   announcement = 'Default announcement text',
+  showStars = false,
+  rating = 5,
 }: FlexAnnouncementBarProps) {
   return (
     <div className="p-3 bg-[#D1E3FB] text-center">
-      <p className="font-medium text-[15px]">{announcement}</p>
+      <div className="flex items-center justify-center gap-1">
+        {showStars && <StarRating rating={rating} />}
+        <p className="font-medium text-[15px]">{announcement}</p>
+      </div>
     </div>
   );
 }
