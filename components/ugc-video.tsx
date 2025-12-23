c876182888eadb043f182a1ffe2ca6a496170a12
Ian Greenfield <iangreenfield@MacBookPro.lan>
2025-12-23 13:30:02 -0600

    fix ugc video top cutoff issue

--- components/ugc-video.tsx
+++ components/ugc-video.tsx
@@ -60,7 +60,7 @@ export default function UGCVideo({
         poster={posterUrl}
         preload="auto"
         src="https://player.vimeo.com/progressive_redirect/playback/1119579050/rendition/720p/file.mp4?loc=external&signature=728907c52d41dbb7fc5ffc32281168626d765f5ced762706f737c378cb7ed311"
-        className="w-full h-full object-cover relative z-10"
+        className="w-full h-full object-cover object-top relative z-10"
       />
       <div className="absolute inset-0 w-full h-full bg-black opacity-20 pointer-events-none" />
 
