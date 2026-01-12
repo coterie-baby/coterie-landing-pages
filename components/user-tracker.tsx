e5e71ba8385d1faca49185086f979eb8f2537fff
Ian Greenfield <iangreenfield@MacBookPro.lan>
2026-01-12 13:33:36 -0600

    fingerprintjs fix

--- components/user-tracker.tsx
+++ components/user-tracker.tsx
@@ -5,14 +5,13 @@ import { useEffect } from 'react';
 
 export default function UserTracker() {
   // Use 'extendedResult: true' to get bot detection signals in the Pro version
-  const { data, isLoading } = useVisitorData({ extendedResult: true });
+  const { data } = useVisitorData({ extendedResult: true });
 
   useEffect(() => {
     if (data) {
       // Logic to log or handle bot detection
       // Pro version provides 'data.bot.result' (e.g., "bad", "good", "not_detected")
       console.log('Visitor ID:', data.visitorId);
-      console.log('Bot Detection:', data.bot?.result);
     }
   }, [data]);
 
